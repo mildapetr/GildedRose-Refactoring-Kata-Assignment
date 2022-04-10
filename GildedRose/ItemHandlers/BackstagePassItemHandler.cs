c235827e91474719425ac8b4e5e6ade72edfc694
Milda Petrikaite <Petrikaite.M@gmail.com>
2022-04-10 20:27:56 +0300

    added backstage pass item handler with unit tests

---
+++ GildedRose/ItemHandlers/BackstagePassItemHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GildedRoseKata.ItemHandlers
+{
+    public class BackstagePassItemHandler : IItemHandler
+    {
+        public const int DefaultQualityIncreaseStep = 1;
+        // first period when backstage pass increases in quality
+        public const int FirstQualityBoostSellIn = 10;
+        public const int FirstQualityBoostStepIncrease = 1;
+        // second period when backstage pass increases in quality
+        public const int SecondQualityBoostSellIn = 5;
+        public const int SecondQualityBoostStepIncrease = 1;
+
+        public Item Update(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+                return item;
+            }
+
+            int qualityIncreaseStep = DefaultQualityIncreaseStep;
+            if (item.SellIn <= FirstQualityBoostSellIn)
+            {
+                qualityIncreaseStep += FirstQualityBoostStepIncrease;
+            }
+            if (item.SellIn <= SecondQualityBoostSellIn)
+            {
+                qualityIncreaseStep += SecondQualityBoostStepIncrease;
+            }
+
+            item.Quality = Math.Min(item.Quality + qualityIncreaseStep, ItemConstants.MaxNonLegendaryItemQuality);
+
+            return item;
+        }
+    }
+}
---
+++ GildedRoseTests/ItemHandlers/BackstagePassItemHandlerTests.cs
@@ -0,0 +1,93 @@
+﻿using GildedRoseKata;
+using GildedRoseKata.ItemHandlers;
+using Xunit;
+
+namespace GildedRoseTests.ItemHandlers
+{
+    public class BackstagePassItemHandlerTests
+    {
+        private readonly BackstagePassItemHandler _handler;
+
+        public BackstagePassItemHandlerTests()
+        {
+            _handler = new BackstagePassItemHandler();
+        }
+
+        [Fact]
+        public void Update_ItemWithNonMaxQualityAndSellInBeforeFirstBoost_IncresesQualityBy1()
+        {
+            // arrange
+            var sellIn = BackstagePassItemHandler.FirstQualityBoostSellIn + 2;
+            var item = new Item { Name = "foo", SellIn = sellIn, Quality = 1 };
+
+            // act
+            var result = _handler.Update(item);
+
+            // assert
+            Assert.Equal(2, result.Quality);
+            Assert.Equal(sellIn - 1, result.SellIn);
+        }
+
+        [Fact]
+        public void Update_ItemWithNonMaxQualityAndSellInBetweenFirstAndSecondBoost_IncresesQualityWithFirstBoostBonus()
+        {
+            // arrange
+            var sellIn = BackstagePassItemHandler.FirstQualityBoostSellIn;
+            var item = new Item { Name = "foo", SellIn = sellIn, Quality = 1 };
+
+            // act
+            var result = _handler.Update(item);
+
+            // assert
+            Assert.Equal(2 + BackstagePassItemHandler.FirstQualityBoostStepIncrease, result.Quality);
+            Assert.Equal(sellIn - 1, result.SellIn);
+        }
+
+        [Fact]
+        public void Update_ItemWithNonMaxQualityAndPositiveSellInAfterSecondBoost_IncresesQualityWithSecondBoostBonus()
+        {
+            // arrange
+            var sellIn = BackstagePassItemHandler.SecondQualityBoostSellIn;
+            var item = new Item { Name = "foo", SellIn = sellIn, Quality = 1 };
+            var expectedBoost = BackstagePassItemHandler.FirstQualityBoostStepIncrease + BackstagePassItemHandler.SecondQualityBoostStepIncrease;
+
+            // act
+            var result = _handler.Update(item);
+
+            // assert
+            Assert.Equal(2 + expectedBoost, result.Quality);
+            Assert.Equal(sellIn - 1, result.SellIn);
+        }
+
+        [Fact]
+        public void Update_ItemWithNonPositiveSellIn_QualityDropsTo0()
+        {
+            // arrange
+            var item = new Item { Name = "foo", SellIn = 0, Quality = 1 };
+
+            // act
+            var result = _handler.Update(item);
+
+            // assert
+            Assert.Equal(0, result.Quality);
+            Assert.Equal(-1, result.SellIn);
+        }
+
+        [Theory]
+        [InlineData(BackstagePassItemHandler.SecondQualityBoostSellIn)]
+        [InlineData(BackstagePassItemHandler.FirstQualityBoostSellIn)]
+        [InlineData(BackstagePassItemHandler.FirstQualityBoostSellIn + 2)]
+        public void Update_ItemWithMaxQualityAndPositiveSellIn_QualityRemainsMax(int sellIn)
+        {
+            // arrange
+            var item = new Item { Name = "foo", SellIn = sellIn, Quality = ItemConstants.MaxNonLegendaryItemQuality };
+
+            // act
+            var result = _handler.Update(item);
+
+            // assert
+            Assert.Equal(ItemConstants.MaxNonLegendaryItemQuality, result.Quality);
+            Assert.Equal(sellIn - 1, result.SellIn);
+        }
+    }
+}
