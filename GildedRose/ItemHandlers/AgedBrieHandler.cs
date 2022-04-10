ef110c54466a00d0ce07fe75208e58086b52cd5e
Milda Petrikaite <Petrikaite.M@gmail.com>
2022-04-10 19:50:03 +0300

    updated aged brie handler to have increase step constant

--- GildedRose/ItemHandlers/AgedBrieHandler.cs
+++ GildedRose/ItemHandlers/AgedBrieHandler.cs
@@ -4,11 +4,13 @@ namespace GildedRoseKata.ItemHandlers
 {
     public class AgedBrieHandler : IItemHandler
     {
+        public const int DefaultQualityIncreaseStep = 1;
+
         public Item Update(Item item)
         {
             item.SellIn--;
 
-            var qualityIncreaseStep = item.SellIn < 0 ? 2 : 1;
+            var qualityIncreaseStep = item.SellIn < 0 ? DefaultQualityIncreaseStep * 2 : DefaultQualityIncreaseStep;
             item.Quality = Math.Min(item.Quality + qualityIncreaseStep, ItemConstants.MaxNonLegendaryItemQuality);
 
             return item;
