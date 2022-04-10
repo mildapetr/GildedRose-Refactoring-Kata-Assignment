ce70158d6aa07e6870758b3e58302691af3a6fd1
Milda Petrikaite <Petrikaite.M@gmail.com>
2022-04-10 19:51:33 +0300

    updated common item handler with decrese step constant

--- GildedRose/ItemHandlers/CommonItemHandler.cs
+++ GildedRose/ItemHandlers/CommonItemHandler.cs
@@ -4,11 +4,13 @@ namespace GildedRoseKata.ItemHandlers
 {
     public class CommonItemHandler : IItemHandler
     {
+        public const int DefaultQualityDecreaseStep = 1;
+
         public Item Update(Item item)
         {
             item.SellIn--;
 
-            var qualityDecreseStep = item.SellIn < 0 ? 2 : 1;
+            var qualityDecreseStep = item.SellIn < 0 ? DefaultQualityDecreaseStep * 2 : DefaultQualityDecreaseStep;
             item.Quality = Math.Max(item.Quality - qualityDecreseStep, 0);
 
             return item;
